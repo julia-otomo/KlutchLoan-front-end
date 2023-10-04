e7e309a76e440f6723ce0f86ee2d32755a579b00
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-10-04 14:54:48 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+import logo from "../../assets/logo.svg";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="w-screen h-[59px] bg-brand-1 flex items-center justify-center">
+      <Image src={logo} alt="" />
+    </header>
+  );
+};
+
+export default Header;
